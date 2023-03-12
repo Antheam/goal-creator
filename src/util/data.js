9f9703602ba8256cbce964d888ddd8039e44d5fa
ANTHEAM <ANTHEA.MERTON@GMAIL.COM>
2023-03-12 22:25:33 +0000

    Added mock data

---
+++ src/util/data.js
@@ -0,0 +1,16 @@
+const todos ={
+    
+taskOne:{name: "learn React",
+description: "Learn React and build projects to improve my skills",
+hours: 40,
+completed: false},
+taskTwo:{name: "learn React",
+description: "Learn React and build projects to improve my skills",
+hours: 40,
+completed: false},
+taskThree:{name: "learn React",
+description: "Learn React and build projects to improve my skills",
+hours: 40,
+completed: false}
+
+}
